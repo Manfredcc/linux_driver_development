95aafb6c1183b0d72e3d4e3c6dbc1f352c8329a0
Manfred <Manfred@y.com>
2023-04-10 21:50:03 +0800

    Implement basic character device framework

---
+++ basic_driver/01_char_dev/README.md
@@ -0,0 +1,33 @@
+# Character device driver : Egoist
+
+| Author  | Date     | Description                                                  |
+| ------- | -------- | ------------------------------------------------------------ |
+| Manfred | 2023/4/8 | First release : The basic character device framework is implemented |
+
+After running `make` in the terminal, there will be one module named as follow:
+
+- ego_char.ko
+
+You can specify the numbers of devices that you want to create by `sudo insmod ego_char.ko number=x`, after loading the module, there will be `/dev/egoist_0, /dev/egoist_1, /dev/egoist_2 ... ...`, there will also be a class created for these devices:`/sys/class/ego_class`.
+
+If you load the driver directly, there will be a log:`Awesome` which show you do the things upon successfully.
+
+```shell
+[Ego]ego_char_init Egoist:Awesome!
+```
+
+After you unload the module, there will be a log as follow:
+
+```shell
+[Ego]ego_char_exit Egoist:See you someday!
+```
+
+> If you don't want to see this log again, you can comment the hard code out as below:
+>
+> ```c
+> // #define DEBUG    /* hard-code control debugging */
+> ```
+
+---
+
+Anyway, after you created the module completely, it's time to test thing with it. What you need to do is just operate the file node `/dev/egoist_x`
---
+++ basic_driver/01_char_dev/ego_char.c
@@ -0,0 +1,174 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/cdev.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
+
+/*============================================================
+            GENRATE A SIMPLE CHARACTER DEVICE
+
+GENERAL DESCRIPTION
+    This driver which implement a simple char device
+
+    When              Who             What, Where, Why
+    ----------        ---             ------------------------
+    2023/04/08        Manfred         Initial reversion
+
+==============================================================*/
+
+static int count = 1;   /* Numbers of char devices */
+module_param(count, int, S_IRUGO);
+
+#define DEBUG    /* hard-code control debugging */
+
+#ifdef DEBUG
+#define ego_debug(format, ...)   \
+            pr_err("[Ego]%s "format, __func__, ##__VA_ARGS__)
+#else
+#define ego_debug(format, ...)
+#endif
+
+struct egoist {
+    struct cdev cdev;
+    struct class *ego_class;
+    struct device *ego_device;
+    int major;
+    int minor;
+    dev_t devt;
+    char *dev_name;
+
+    void *ego_data;
+};
+static struct egoist *chip;
+
+/* Implement OPS */
+static int ego_open(struct inode *inode, struct file *filep)
+{
+    ego_debug("call %s\n", __func__);
+
+    return 0;
+}
+
+static ssize_t ego_read(struct file *filp, char __user *buf, size_t count, loff_t *offset)
+{
+    ego_debug("call %s\n", __func__);
+
+    return count;
+}
+
+static ssize_t ego_write(struct file *filp, const char __user *buf, size_t count, loff_t *offset)
+{
+    ego_debug("call %s\n", __func__);
+
+    return count;
+}
+
+static int ego_release(struct inode *inode, struct file *filep)
+{
+    ego_debug("call %s\n", __func__);
+
+    return 0;
+}
+
+static const struct file_operations ego_ops = {
+    .owner    =   THIS_MODULE,
+    .open     =   ego_open,
+    .read     =   ego_read,
+    .write    =   ego_write,
+    .release  =   ego_release,
+};
+
+static int __init ego_char_init(void)
+{
+    int ret;
+    int cnt;
+
+    chip = kzalloc(sizeof(*chip), GFP_KERNEL);
+    if (!chip) {
+        ego_debug("Failed to alloc mem for egoist[self_struct]\n");
+        return ENOMEM;
+    }
+    /*----------------------------------------------
+    Step -1- Allocate a char device number for [cdev]
+            Take the dynamic method API
+                alloc_chrdev_region
+    ------------------------------------------------*/
+    ret = alloc_chrdev_region(&chip->devt, 0, count, "ego_char");
+    if (ret) {
+        ego_debug("Failed to allocate a char device number for [ego_char]\n");
+        return EBUSY;
+    }
+
+    chip->major = MAJOR(chip->devt);
+    /*----------------------------------------------
+    Step -2- Initialize [cdev] and associate it with
+    the [File OPS]
+            API:cdev_init
+    ------------------------------------------------*/
+    cdev_init(&chip->cdev, &ego_ops);
+    chip->cdev.owner = THIS_MODULE;
+
+    /*----------------------------------------------
+    Step -3- Add the char-dev[ego_char] to the system
+    the [File OPS]
+            API:cdev_add
+    ------------------------------------------------*/
+    ret = cdev_add(&chip->cdev, chip->devt, count);
+    if (ret) {
+        ego_debug("Failed to add [ego_char] to system\n");
+        return EBUSY;
+    }
+
+    /* Create a device class, visible in /sys/class */
+    chip->ego_class = class_create(THIS_MODULE, "egoist");
+    if (IS_ERR(chip->ego_class)) {
+        ego_debug("Failed to create egoist class\n");
+        unregister_chrdev_region(chip->devt, count);
+        
+        return PTR_ERR(chip->ego_class);
+    }
+
+    /* Create a device, visible in /dev/ */
+    chip->dev_name = "egoist_";
+    for (cnt = 0; cnt < count; cnt++) {
+
+        chip->ego_device = device_create(chip->ego_class, NULL,
+                                        chip->devt + cnt, NULL,
+                                        "%s%d", chip->dev_name, cnt);
+        if (IS_ERR(&chip->ego_device)) {
+            ego_debug("Faile to create ego device\n");
+            class_destroy(chip->ego_class);
+            unregister_chrdev_region(chip->devt, count);
+        
+            return PTR_ERR(chip->ego_device);
+        }
+    }
+
+    ego_debug("Egoist:Awesome!\n");
+
+    return 0;
+}
+
+static void __exit ego_char_exit(void)
+{
+    int cnt;
+
+    unregister_chrdev_region(chip->devt, count);
+    for (cnt = 0; cnt < count; cnt++) {
+        device_destroy(chip->ego_class, MKDEV(chip->major, cnt));
+    }
+    cdev_del(&chip->cdev);
+    class_destroy(chip->ego_class);
+
+    ego_debug("Egoist:See you someday!\n");
+}
+
+module_init(ego_char_init);
+module_exit(ego_char_exit);
+
+MODULE_PARM_DESC(count, "Numbers of char-dev that need to be register");
+MODULE_AUTHOR("Manfred <1259106665@qq.com>");
+MODULE_DESCRIPTION("This driver which implement a simple char device");
+MODULE_LICENSE("GPL");
